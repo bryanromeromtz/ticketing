50d0b950d6a726dbd9dcc6755eca6b8f2037311c
Madzilla <cryptomtz@hotmail.com>
2023-08-14 17:58:47 -0600

    adding ingress service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const port: number = 3000;
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.sendStatus(200).json({ ok: "Esta bien" });
+});
+
 app.listen(port, () => {
   console.log(`Server on port ${port}!`);
 });
